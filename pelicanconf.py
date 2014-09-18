7ff66d3d46628c7fc3f1535337fb5a3fbb6ddaa6
adrin <adrin.jalali@gmail.com>
2014-09-18 15:16:36 +0200

    working from leni

--- pelicanconf.py
+++ pelicanconf.py
@@ -43,7 +43,7 @@ MENUITEMS=(('Get in Touch', '/pages/get-in-touch'),
 
 THEME="pelican-themes/pelican-elegant"
 
-PLUGIN_PATH = ['pelican-plugins']
+PLUGIN_PATHS = ['pelican-plugins']
 PLUGINS = ['sitemap', 'extract_toc', 'tipue_search']
 DISQUS_SITENAME = u'adrin'
 DISQUS_SITEURL= u'adrin.info'
@@ -58,3 +58,5 @@ AUTHOR_SAVE_AS = ''
 SITE_LICENSE='<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.'
 
 MAILCHIMP_FORM_ACTION=True
+
+SITEMAP={'format':'xml'}
